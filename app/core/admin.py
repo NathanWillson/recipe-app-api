928527380c7e8da1b9d98204485aef4dece70739
NathanWillson <jack4asd@gmail.com>
2022-11-28 01:18:37 +0530

    setup django admin 57

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,71 @@
-from django.contrib import admin # noqa
+# from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    # 56
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            # 'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+
+# underscore.
+# And basically what this does is it integrates with the Django translation
+# system.So if you do change the language of Django and you want to change it
+# for everywhere in your project, then you can do it in the configuration and
+# it means any way you use this translation shortcut here,
+# it's going to automatically translate the text, which is really useful and
+# this is just best practice.
+# We're not actually going to be implementing any translation in our code, but
+# it's always best practice to do this just in case you do need to integrate
+# translation later on in the project. Okay, so the reason we're integrating
+# the translation here is because we want to translate the values
+# that we're putting in.
+# So here when we have non we're just not paying a title bar in this one,
+# we are actually going to put a title.
+# So I'll take underscore which kind of calls this get lazy function here.
+# We name it as underscore.
+# This is just the standard convention that Django uses.
+# You could name this as translate or something.
+# That just means that you need to type, translate or type the function name
+# every time you want to call it.
--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,63 @@
-from django.db import models # noqa
+# from django.db import models # noqa
 
 
 # Create your models here.
+
+"""
+Database models.
+"""
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        # user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        # set the encrypted password.
+        user.save(using=self._db)
+        # support adding multiple databases if you choose to add multiple
+        # databases to ur project
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    # And this is how we replace the username
+    # default field that comes with the
+    # default user model to our custom email field.
+
+
+"""here **extra_fields === what that means is that we can provide
+        keyword arguments,any number of keyword arguments that
+        will be passed into our model.
+        This is useful when you define additional fields,
+        for example, a name, you can pass it as an extra
+        field and that will be automatically created
+        when the user model is created."""
